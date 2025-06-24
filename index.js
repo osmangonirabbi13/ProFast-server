911e1eb033f1bee41da64823e3bd46e8123721c9
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-06-24 15:37:50 +0600

    mongoDB connect

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // Load environment variables from .env file
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.jclxqqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("parcelDB"); // database name
+    const parcelCollection = db.collection("parcels");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Sample route
 app.get("/", (req, res) => {
   res.send("Parcel Server is running");
